b24ba8ad0f9a3aeb90eb99570c27ed8d8b248a31
Fatimah Vakily <fvaki076@uottawa.ca>
2021-07-17 00:34:04 -0400

    added homepage text

--- SEG3125-Module8and9-CustomWebsite/booknook/src/App.js
+++ SEG3125-Module8and9-CustomWebsite/booknook/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import bookphoto from './assets/homepagebooks.png';
+import bookphoto from './assets/homepagebooks2.png';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&family=Playfair+Display:wght@700&display=swap');
+</style> 
 
 function App() {
   return (
@@ -9,7 +12,13 @@ function App() {
      
     <Navbar/>
 
+    <div>
     <img src={bookphoto} className="bookphotoClass" alt="Welcome"></img> 
+    <div class="hometext1">Welcome to Book Nook!</div>
+    <div class="hometext2">Feel free to browse our library, make reviews on our discussion board, or sign up to join our book club and gain access to our book club virtual meetings!</div>
+
+    </div>
+
     <Footer />
     </div>
   );
