70000d7609ee3b2a53c429caad93f4daa0df2740
Fatimah Vakily <fvaki076@uottawa.ca>
2021-07-16 23:59:03 -0400

    added background on home page

---
+++ SEG3125-Module8and9-CustomWebsite/README.md
@@ -0,0 +1,3 @@
+Implementation of a website for a book club by Fatimah V.
+
+Images of books came from: https://wallpapercave.com/books-wallpapers
--- SEG3125-Module8and9-CustomWebsite/booknook/src/App.js
+++ SEG3125-Module8and9-CustomWebsite/booknook/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import bookphoto from './assets/homepagebooks.png';
 
 function App() {
   return (
     <div className="App">
      
     <Navbar/>
+
+    <img src={bookphoto} className="bookphotoClass" alt="Welcome"></img> 
     <Footer />
     </div>
   );
